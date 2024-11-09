f9e62e563a34751de440c6179b1ed319e26648cf
kylermatheson <kyle2003km1@gmail.com>
2024-11-09 15:14:59 -0700

    finish

--- W8/register.js
+++ W8/register.js
@@ -1,29 +1,82 @@
+let count = 1;
 
 function participantTemplate() {
-    let count = 0;
+
     count++;  // Increment the count variable directly
     console.log(count);  // Log the updated value of count
+    return`    
+    <section class="participant${count}">
+    <p>Participant ${count}</p>
+    <div class="item">
+      <label for="fname"> First Name<span>*</span></label>
+      <input id="fname" type="text" name="fname" value="" required />
+    </div>
+    <div class="item activities">
+      <label for="activity">Activity #<span>*</span></label>
+      <input id="activity" type="text" name="activity" />
+    </div>
+    <div class="item">
+      <label for="fee">Fee ($)<span>*</span></label>
+      <input id="fee" type="number" name="fee" />
+    </div>
+    <div class="item">
+      <label for="date">Desired Date <span>*</span></label>
+      <input id="date" type="date" name="date" />
+    </div>
+    <div class="item">
+      <p>Grade</p>
+      <select>
+        <option selected value="" disabled selected></option>
+        <option value="1">1st</option>
+        <option value="2">2nd</option>
+        <option value="3">3rd</option>
+        <option value="4">4th</option>
+        <option value="5">5th</option>
+        <option value="6">6th</option>
+        <option value="7">7th</option>
+        <option value="8">8th</option>
+        <option value="9">9th</option>
+        <option value="10">10th</option>
+        <option value="11">11th</option>
+        <option value="12">12th</option>
+      </select>
+    </div>
+  </section>`
+
 }
-
-document.getElementById("add").addEventListener("click", participantTemplate);
-
 
 function submitForm(event) {
     event.preventDefault();
-    // do the rest of the stuff
-    }
+
+    const adultName = document.getElementById('adult_name').value;
+    const numParticipants = count;
+    const totalFee = totalFees();
+
+    const summaryElement = document.getElementById('summary');
+    summaryElement.innerHTML = `Thank you ${adultName} for registering. You have registered ${numParticipants} participants and owe $${totalFee} in fees.`;
+
+    document.querySelector('.testbox form').style.display = "none";
+    summaryElement.style.display = "block";
+}
+
+
+submit = document.getElementById("submitButton").addEventListener("click", submitForm)
 
 function totalFees() {
-    // the selector below lets us grab any element that has an id that begins with "fee"
     let feeElements = document.querySelectorAll("[id^=fee]");
-    console.log(feeElements);
-    // querySelectorAll returns a NodeList. It's like an Array, but not exactly the same.
-    // The line below is an easy way to convert something that is list-like to an actual Array so we can use all of the helpful Array methods...like reduce
-    // The "..." is called the spread operator. It "spreads" apart the list, then the [] we wrapped it in inserts those list items into a new Array.
     feeElements = [...feeElements];
-    // sum up all of the fees. Something like Array.reduce() could be very helpful here :) Or you could use a Array.forEach() as well.
-    // Remember that the text that was entered into the input element will be found in the .value of the element.
-        
-    // once you have your total make sure to return it!
-        
-        }
+    const total = feeElements.reduce((sum, el) => sum + Number(el.value), 0);
+    return total;
+}
+
+
+
+document.getElementById("add").addEventListener("click", function () {
+    const template = participantTemplate(count);
+    document.getElementById("add").insertAdjacentHTML("beforebegin", template);
+});
+
+
+
+
+
